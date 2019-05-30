d4eb9b4e10941d0489ad2df7ace4d3ee02bcfccf
Yijiehuahua <345239814@qq.com>
2019-05-30 21:09:11 +0800

    finish this work by a simply way

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
 module.exports = function main(inputs) {
-    // write your code here...
+  let price;
+  let transfee = 0.8;
+  let parktime = 0.25;
+    if(inputs.distance <= 2 && inputs.parkTime){
+      price = Math.round(6 + inputs.parkTime * parktime);
+      return price;
+    } else if(inputs.distance > 2 && inputs.distance <= 8 && inputs.parkTime){
+      price = Math.round(6 + (inputs.distance - 2) * transfee + inputs.parkTime * parktime);
+      return price;
+    }else if(inputs.distance > 8 && inputs.parkTime){
+      price = Math.round(6 + (8 - 2) * transfee + (inputs.distance - 8) * (transfee/2 + transfee) + inputs.parkTime * parktime);
+      return price;
+    } else if(inputs.distance > 8){
+      price = Math.round(6 + (8 - 2) * transfee + (inputs.distance - 8) * (transfee/2 + transfee));
+      return price;
+    }
     return "price";
 };
